39b6602cd05e37b3c70b5da79b9c774579b9c16b
yanjiahui123 <964944524@qq.com>
2020-07-03 22:19:19 +0800

    day5test

---
+++ day5/makefile/main.c
@@ -0,0 +1,10 @@
+#include <func.h>
+void print();
+int sum(int a,int b);
+int main()
+{
+    print();
+    sum(3, 4);
+    return 0;
+}
+
---
+++ day5/zuoye/main.c
@@ -0,0 +1,15 @@
+#include <func.h>
+
+int multi(int a,int b);
+void print()
+{
+    char *p;
+    *p = 's';
+}
+int main()
+{
+    printf("multi=%d\n",multi(3,4)); 
+    print();
+    return 0;
+}
+
